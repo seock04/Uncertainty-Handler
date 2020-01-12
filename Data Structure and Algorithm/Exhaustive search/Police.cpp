9a475e676e6f18cd430823b1b69900f372fc4dd1
Hoseock Choi <seock04@gmail.com>
2020-01-13 07:22:33 +0900

    use memory to increase the performance

--- Data Structure and Algorithm/Exhaustive search/Police.cpp
+++ Data Structure and Algorithm/Exhaustive search/Police.cpp
@@ -11,16 +11,24 @@ int N, W;
 int Event[MAX_EVENT][2];
 int ret = 0x7fffffff;
 
+int Distance[MAX_WAY][MAX_WAY][MAX_EVENT][MAX_EVENT] = { 0, };
+
 int solve(int x1, int y1, int x2, int y2, int w, int dis)
 {
 	if (w == W) {
 		return dis;
 	}
+	if (Distance[x1][y1][Event[w][0]][Event[w][1]] == 0) {
+		Distance[x1][y1][Event[w][0]][Event[w][1]] = mABS(Event[w][0] - x1) + mABS(Event[w][1] - y1);
+	}
+	if (Distance[x2][y2][Event[w][0]][Event[w][1]] == 0) {
+		Distance[x2][y2][Event[w][0]][Event[w][1]] = mABS(Event[w][0] - x2) + mABS(Event[w][1] - y2);
+	}
 
-	int dis1 = mABS(Event[w][0] - x1) + mABS(Event[w][1] - y1);
-	int dis2 = mABS(Event[w][0] - x2) + mABS(Event[w][1] - y2);
+	//int dis1 = mABS(Event[w][0] - x1) + mABS(Event[w][1] - y1);
+	//int dis2 = mABS(Event[w][0] - x2) + mABS(Event[w][1] - y2);
 
-	return min(solve(Event[w][0], Event[w][1], x2, y2, w + 1, dis + dis1), solve(x1, y1, Event[w][0], Event[w][1], w + 1, dis + dis2));
+	return min(solve(Event[w][0], Event[w][1], x2, y2, w + 1, dis + Distance[x1][y1][Event[w][0]][Event[w][1]]), solve(x1, y1, Event[w][0], Event[w][1], w + 1, dis + Distance[x2][y2][Event[w][0]][Event[w][1]]));
 
 }
 
