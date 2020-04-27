9b72984aaaf89fd22a976a41021f887fe89c75fa
Hoseock Choi <seock04@gmail.com>
2020-04-28 06:22:32 +0900

    updae tiles with memoization

--- Data Structure and Algorithm/Relation based algorithm/tiles.cpp
+++ Data Structure and Algorithm/Relation based algorithm/tiles.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+#define MAXN 100001
+
 using namespace std;
 int M;
+
+int DT[MAXN];
 
 int solve(int n)
 {
@@ -12,6 +16,18 @@ int solve(int n)
 	}
 }
 
+int solve_DT(int n)
+{
+	if (-1 != DT[n]) return DT[n];
+
+	if (n == 1) return DT[n] = 1;
+	else if (n == 2) return DT[n] = 3;
+	else {
+		return DT[n] = (2 * solve(n - 2) + solve(n - 1)) % M;
+	}
+}
+
+
 int main()
 {
 	int N;
@@ -19,7 +35,11 @@ int main()
 	cin >> N;
 	cin >> M;
 
-	int ret = solve(N);
+	for (int i = 0; i < MAXN; i++) {
+		DT[i] = -1;
+	}
+
+	int ret = solve_DT(N);
 
 	cout << ret % M;
 	
