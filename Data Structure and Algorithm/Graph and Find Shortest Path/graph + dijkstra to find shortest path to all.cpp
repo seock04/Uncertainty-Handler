7b13fcc3af521ca7d48d18493026a2ef423be5c0
Hoseock Choi <seock04@gmail.com>
2020-10-17 21:15:54 +0900

    add graph and dijkstra practice code

---
+++ Data Structure and Algorithm/Graph and Find Shortest Path/graph + dijkstra to find shortest path to all.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+using namespace std;
+
+#define MAX 200
+
+#define INF 9999
+
+int N;
+int map[MAX][MAX];
+int dist[MAX];
+int parent[MAX];
+bool visited[MAX];
+
+int src;
+
+void resetVisited()
+{
+	for (int i = 0; i < N; i++) {
+		visited[i] = false;
+	}
+}
+
+void Init()
+{
+	for (int i = 0; i < N; i++) {
+		dist[i] = INF;
+		parent[i] = i;
+		for (int j = 0; j < N; j++) {
+			map[i][j] = INF;
+		}
+	}
+
+	resetVisited();
+}
+
+int getNearest()
+{
+	int minValue = INF;
+	int minNode = 0;
+
+	for (int i = 0; i < N; i++) {
+		if (!visited[i]
+			&& map[src][i] < minValue) {
+			minValue = map[src][i];
+			minNode = i;
+		}
+	}
+	return minNode;
+}
+
+
+void dijkstra()
+{
+	dist[src] = 0;
+	
+	for (int i = 0; i < N; i++) {
+		int nearest = getNearest();
+		visited[nearest] = true;
+
+		for (int adj = 0; adj < N; adj++) {
+			if (map[nearest][adj] != INF
+				&& dist[adj] > dist[nearest] + map[nearest][adj]) {
+				dist[adj] = dist[nearest] + map[nearest][adj];
+				parent[adj] = nearest;
+			}
+		}
+	}
+}
+
+void printShortestPath()
+{
+	cout << "=======================";
+	for (int i = 0; i < N; i++) {
+		int parentNode = i;
+		while (parentNode != src) {
+			cout << i << " <- ";
+			parentNode = parent[parentNode];
+		}
+		cout << "----------------------------\n";
+	}
+}
+
+int main()
+{
+	cin >> N;
+	
+	int edges;
+	cin >> edges;
+
+	int s, e, v;
+	Init();
+	for (int i = 0; i < edges; i++) {
+		cin >> s >> e >> v;
+		map[s][e] = v;
+		map[e][s] = v;
+	}
+
+	src = 0;
+	dijkstra();
+	printShortestPath();
+
+}
