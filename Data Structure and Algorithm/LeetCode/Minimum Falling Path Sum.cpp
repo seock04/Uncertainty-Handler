a8157f506d74e94db7ece5f9263a1f37659d0cb0
seock04 <seock04@gmail.com>
2021-02-07 18:49:42 +0900

    update

--- Data Structure and Algorithm/LeetCode/Minimum Falling Path Sum.cpp
+++ Data Structure and Algorithm/LeetCode/Minimum Falling Path Sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int N;
+    int N, M;
     int dp[101][101];
     int minFallingPathSum(vector<vector<int>>& mt) {
        
+        M = mt.size();
         N = mt[0].size();
-        memset(dp, -1, sizeof dp);
         
         for(int i =0;i<N; ++i){
-            dp[N-1][i] = mt[N-1][i];
+            dp[M-1][i] = mt[M-1][i];
         }
         
-        for(int i = N-2; i >= 0; --i){
+        for(int i = M-2; i >= 0; --i){
             for(int j = 0;j < N; ++j){
                 
                 dp[i][j] = mt[i][j];
                 int mn = mt[i+1][j];
                 
-                if(j-1 > 0){
+                if(j-1 >= 0){
                     mn = min(mn, mt[i+1][j-1]);
                 }
                 
@@ -30,7 +30,7 @@ public:
         }
         int result = INT_MAX;
         for(int i = 0; i<N; i++){
-            result = (dp[0][i] < result) ? dp[0][i] : result;
+            result = min(dp[0][i], result);
         }
         
         return result;
