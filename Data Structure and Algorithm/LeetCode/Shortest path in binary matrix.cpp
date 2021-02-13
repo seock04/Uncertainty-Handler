7b74ce986800c6c8706442929fe484ce9000dcb1
Hoseock Choi <seock04@gmail.com>
2021-02-13 23:33:21 +0900

    Update Shortest path in binary matrix.cpp

--- Data Structure and Algorithm/LeetCode/Shortest path in binary matrix.cpp
+++ Data Structure and Algorithm/LeetCode/Shortest path in binary matrix.cpp
@@ -50,3 +50,62 @@ public:
         
     }
 };
+
+
+// below are using BFS
+class Solution {
+public:
+    int dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+    int dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+    
+    vector<pair<int, int>> queue;
+    int visited[101][101];
+    int N, M;
+    
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+    
+        if(grid.size() == 0 or grid[0].size() == 0) return -1;
+        N = grid.size();
+        M = grid[0].size();
+        cout << "#size "<< N << " " << M << "\n";
+        if(grid[0][0] == 1 or grid[N-1][M-1] == 1) return -1;
+        
+        memset(visited, 0, sizeof visited);
+        
+        visited[0][0] = 1;
+        queue.push_back({0,0});
+        int level = 0;
+        
+        while(queue.size() != 0){
+            cout << "level: " << level << "\n";
+            int size = queue.size();
+            level++;
+            for(int i = 0; i < size; i++){
+                int cury = queue.begin()->first;
+                int curx = queue.begin()->second;
+                queue.erase(queue.begin());
+                cout << "#1 "<< cury << " " << curx << "\n";
+                if(cury == N-1 and curx == M-1) {
+                    return level;
+                }
+                for(int d = 0; d < 8; d++){
+                    int yy = cury + dy[d];
+                    int xx = curx + dx[d];
+                    
+                    if(visited[yy][xx] or yy < 0 or yy >= N or xx < 0 or xx >= M 
+                       or grid[yy][xx] == 1){
+                        continue;
+                    }
+                    cout <<"#2 " << yy << " " << xx << "\n";
+                    queue.push_back({yy, xx});
+                    visited[yy][xx] = 1;
+                }
+            }            
+            
+        }
+        
+        return -1;
+        
+    }
+};
+
