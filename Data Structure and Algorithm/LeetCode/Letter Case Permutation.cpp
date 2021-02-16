d9d8a21a01af36bd6d9f3d7a454ed8a7cf2d15fd
Hoseock Choi <seock04@gmail.com>
2021-02-17 01:30:07 +0900

    Update Letter Case Permutation.cpp

--- Data Structure and Algorithm/LeetCode/Letter Case Permutation.cpp
+++ Data Structure and Algorithm/LeetCode/Letter Case Permutation.cpp
@@ -1,3 +1,5 @@
+
+///////////////////using bit operation ////////////////////////
 class Solution {
 public:
     vector<string> letterCasePermutation(string S) {
@@ -23,5 +25,42 @@ public:
             result.push_back(s);
         }
         return result;
+    }
+};
+
+
+////////////// using backtracking ///////////////////////////
+
+class Solution {
+public:
+    vector<string> letterCasePermutation(string S) {
+       vector<string> result;
+       string s; 
+       permute(S, 0, s, result);
+       return result;
+    }
+    
+    void permute(string S, int index, string & s, vector<string> & res)
+    {
+        if(index >= S.length()){
+            res.push_back(s);
+            return;
+        }
+        
+        char c = S[index];
+        if(isalpha(c)){
+            char cl = tolower(c);
+            string lower = s + cl;
+            
+            char cu = toupper(c);
+            string upper = s + cu;
+            
+            permute(S, index+1, lower, res);
+            permute(S, index+1, upper, res);
+        }
+        else{
+            s+=c;
+            permute(S, index + 1, s, res);
+        }
     }
 };
