bfb8310449c0e936a4c93ca737c196dd5c1c95e0
Hoseock Choi <seock04@gmail.com>
2021-03-06 20:05:18 +0900

    Update Intersection of Two Linked Lists.cpp

--- Data Structure and Algorithm/LeetCode/Intersection of Two Linked Lists.cpp
+++ Data Structure and Algorithm/LeetCode/Intersection of Two Linked Lists.cpp
@@ -9,6 +9,7 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+/*
         unordered_map<ListNode*, int> hashmap;
         
         for(ListNode * iter = headA; iter != nullptr; iter = iter->next){
@@ -22,5 +23,33 @@ public:
         }
         
         return nullptr;
+  */      
+        
+        //or
+        vector<ListNode*> stackA;
+        vector<ListNode*> stackB;
+        
+        while(headA != nullptr){
+            stackA.push_back(headA);
+            cout << stackA.back() << "\n";
+            headA = headA->next;
+        }
+        
+        while(headB != nullptr){
+            stackB.push_back(headB);
+            cout << stackB.back() << "\n";
+            headB = headB->next;
+        }
+        
+        ListNode* result = nullptr;
+        while(!stackA.empty() and !stackB.empty() and stackA.back() == stackB.back()){
+            cout << stackA.back() << " " << stackB.back() << "\n";
+            result = stackA.back();
+            stackA.pop_back();
+            stackB.pop_back();
+        }        
+        
+        return result;          
+            
     }
 };
