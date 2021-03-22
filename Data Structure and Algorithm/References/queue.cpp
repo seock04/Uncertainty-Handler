5996d7b1c8537b1572ab4e44c7038481cdc1b1ab
Hoseock Choi <seock04@gmail.com>
2021-03-22 21:44:51 +0900

    Create queue.cpp

---
+++ Data Structure and Algorithm/References/queue.cpp
@@ -0,0 +1,101 @@
+/*
+2 // Test case
+5 // Data size
+1 2 3 4 5
+5
+5 4 2 3 1
+*/
+
+int front;
+int rear;
+int queue[MAX_N];
+
+void queueInit(void)
+{
+	front = 0;
+	rear = 0;
+}
+
+int queueIsEmpty(void)
+{
+	return (front == rear);
+}
+
+int queueIsFull(void)
+{
+	if ((front + 1) % MAX_N == rear)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int queueEnqueue(int value)
+{
+	if (queueIsFull())
+	{
+		printf("queue is full!");
+		return 0;
+	}
+	queue[front] = value;
+	front++;
+	if (front == MAX_N)
+	{
+		front = 0;
+	}
+
+	return 1;
+}
+
+int queueDequeue(int *value)
+{
+	if (queueIsEmpty()) 
+	{
+		printf("queue is empty!");
+		return 0;
+	}
+	*value = queue[rear];
+	rear++;
+	if (rear == MAX_N)
+	{
+		rear = 0;
+	}
+	return 1;
+}
+
+int main(int argc, char* argv[])
+{
+	int T;
+	int N;
+
+	scanf("%d", &T);
+
+	for (int test_case = 1; test_case <= T; test_case++)
+	{
+		scanf("%d", &N);
+
+		queueInit();
+		for (int i = 0; i < N; i++)
+		{
+			int value;
+			scanf("%d", &value);
+			queueEnqueue(value);
+		}
+
+		printf("#%d ", test_case);
+
+		while (!queueIsEmpty())
+		{
+			int value;
+			if (queueDequeue(&value) == 1)
+			{
+				printf("%d ", value);
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
